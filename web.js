f11d3154ee1ac1c1425c896de0e5292567ca172f
Sotiris Bantas <sotiris.bantas@gmail.com>
2013-07-11 05:37:34 +0000

    Interim

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+var fs = require('fs');
+
+var readin = fs.readFile('./index.html');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!);
+  response.send(readin);
 });
 
 var port = process.env.PORT || 5000;
