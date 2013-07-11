bbc0240032b7d9f267c80db63b70f761ec92c0d0
Sotiris Bantas <sotiris.bantas@gmail.com>
2013-07-11 05:47:11 +0000

    Reading from index.html correct

--- web.js
+++ web.js
@@ -1,13 +1,13 @@
 var fs = require('fs');
 var buffer = new Buffer( fs.readFileSync('./index.html') , "utf-8")
-console.log( buffer.toString('utf-8'));
+//console.log( buffer.toString('utf-8'));
 
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send("Hello");
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
